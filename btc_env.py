Scaling: prior fix too slow; pre-scale observations (batch), online-scale rewards/stationary

btc_env.py
@@ -74,7 +74,7 @@ class Scaler(object):
         # Skip every few fittings. Each individual doesn't contribute a whole lot anyway, and costs a lot
         return self.done or (self.i % self.SKIP != 0 and self.i > self.SKIP)
 
-    def transform(self, row, kind):
+    def transform(self, input, kind, matrix=False):
         # this is awkward; we only want to increment once per step, but we're calling this fn 3x per step (once
         # for series, once for stationary, once for reward). Explicitly saying "only increment for one of those" here.
         # Using STATIONARY since SERIES might be called once per timestep in a conv window. TODO Seriously awkward
@@ -82,11 +82,16 @@ class Scaler(object):
 
         scaler, data = self.scalers[kind], self.data[kind]
         if self._should_skip():
-            return scaler.transform([row])[-1]
+            if matrix: return scaler.transform(input)
+            else: return scaler.transform([input])[-1]
         # Fit, transform, return
-        data.append(row)
-        ret = scaler.fit_transform(data)[-1]
-        if kind == self.REWARD: ret = ret[0]
+        if matrix:
+            self.data[kind] += input.tolist()
+            ret = scaler.fit_transform(data)[-input.shape[0]:]
+        else:
+            data.append(input)
+            ret = scaler.fit_transform(data)[-1]
+            if kind == self.REWARD: ret = ret[0]
         if self.i >= self.STOP_AT and not self.done:
             self.done = True
             # Clear up memory, fitted scalers have all the info we need. stop=True only needed in one of these functions
@@ -237,7 +242,10 @@ class BitcoinEnv(Environment):
 
         states = np.nan_to_num(np.column_stack(columns))
         prices = df[data.target].values
-        # Note: don't scale/normalize here, since we'll normalize w/ self.price/step_acc.cash after each action
+
+        if self.hypers.scale:
+            states = self.scaler.transform(states, Scaler.SERIES, matrix=True)
+
         return states, prices
 
     def use_dataset(self, mode, no_kill=False):
@@ -272,22 +280,25 @@ class BitcoinEnv(Environment):
     def _get_next_state(self, i, cash, value, repeats):
         next_series = self.observations[i]
         next_stationary = [cash, value, repeats]
-        if self.hypers.repeat_last_state:
-            next_stationary += next_series.tolist()
         if self.hypers.scale:
-            next_series = self.scaler.transform(next_series, Scaler.SERIES)
-            next_stationary = self.scaler.transform(next_stationary, Scaler.STATIONARY)
+            ## This is already scaled in self._xform_data()
+            # next_series = self.scaler.transform(next_series, Scaler.SERIES)
+            next_stationary = self.scaler.transform(next_stationary, Scaler.STATIONARY).tolist()
 
         if self.conv2d:
+            if self.hypers.repeat_last_state:
+                next_stationary += next_series.tolist()
+
             # Take note of the +1 here. LSTM uses a single index [i], which grabs the list's end. Conv uses a window,
             # [-something:i], which _excludes_ the list's end (due to Python indexing). Without this +1, conv would
             # have a 1-step-behind delayed response.
-            window = []
-            for j in range(i - self.hypers.step_window + 1, i):
-                step = self.observations[i]
-                window.append(self.scaler.transform(step, Scaler.SERIES) if self.hypers.scale else step)
-            window.append(next_series)
-            # window = self.observations[i - self.hypers.step_window + 1:i + 1]
+            window = self.observations[i - self.hypers.step_window + 1:i + 1]
+
+            ## This is already scaled in self._xform_data()
+            # if self.hypers.scale:
+            #     window = self.scaler.transform(window, self.scaler.SERIES, matrix=True)
+            # window = np.concatenate([window, [next_series]], axis=0)
+
             next_series = np.expand_dims(window, axis=1)
         return dict(series=next_series, stationary=next_stationary)
 